5213ee27ac917f4cabdea33299ca7f0e738a2cc5
Darren Cauthon <darren@cauthon.com>
2015-09-02 14:57:34 -0500

    Look for a sha if no tag could be found.

--- lib/gersion.rb
+++ lib/gersion.rb
@@ -4,7 +4,8 @@ require "gersion/version"
 module Gersion
 
   def self.current_version
-    Gersion::Bash.run('git tag --points-at HEAD').split("\n")[0]
+    result = Gersion::Bash.run('git tag --points-at HEAD').split("\n")[0]
+    result || Gersion::Bash.run('git rev-list -1 HEAD').split("\n")[0]
   end
 
 end
--- spec/gersion_spec.rb
+++ spec/gersion_spec.rb
@@ -4,18 +4,42 @@ describe Gersion do
 
   describe "current version" do
 
+    before do
+      Gersion::Bash.stubs(:run)
+        .with('git tag --points-at HEAD')
+        .returns head_tag_check_result
+    end
+
     describe "the current version is bound to a tag" do
 
       let(:tag) { random_string }
 
-      before do
-        Gersion::Bash.stubs(:run)
-          .with('git tag --points-at HEAD')
-          .returns "#{tag}\n#{random_string}"
-      end
+      let(:head_tag_check_result) { "#{tag}\n#{random_string}" }
 
       it "should return the tag" do
         Gersion.current_version.must_equal tag
+      end
+
+    end
+
+    describe "the current version is NOT bound to a tag" do
+
+      let(:head_tag_check_result) { "" }
+
+      describe "but it has a current sha key" do
+
+        let(:sha) { random_string }
+
+        before do
+          Gersion::Bash.stubs(:run)
+            .with('git rev-list -1 HEAD')
+            .returns "#{sha}\n#{random_string}"
+        end
+
+        it "should return the sha" do
+          Gersion.current_version.must_equal sha
+        end
+
       end
 
     end
